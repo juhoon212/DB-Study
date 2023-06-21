b8eb3ed0be4de228e910a5bfc4f1069b00856cb3
juhoon212 <ws5501@naver.com>
2023-06-22 00:27:55 +0900

    Initial commit

---
+++ src/main/java/com/example/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package com.example.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:top://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+}
---
+++ src/main/java/com/example/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,68 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.connection.DBConnectionUtil;
+import com.example.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, null);
+        }
+    }
+
+    private void close(Connection conn, Statement stmt, ResultSet rs) {
+
+        if(rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+
+
+
+        if(stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if(conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+
+
+}
